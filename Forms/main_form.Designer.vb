53aa193d9c9ed7ff45fae2a3cd570105ce7921fc
SergeyT <0tx0rx@gmail.com>
2022-07-30 21:13:23 +0700

    Start

---
+++ Forms/main_form.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.ЗакрытьAIMPToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ВыходToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnPause = New System.Windows.Forms.Button()
+        Me.btnPrev = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.TrackBar1.Location = New System.Drawing.Point(12, 42)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Minimum = 30
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.TrackBar1.Size = New System.Drawing.Size(144, 45)
+        Me.TrackBar1.TabIndex = 9
+        Me.TrackBar1.TabStop = False
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.None
+        Me.TrackBar1.Value = 100
+        '
+        'ЗакрытьAIMPToolStripMenuItem
+        '
+        Me.ЗакрытьAIMPToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.ЗакрытьAIMPToolStripMenuItem.Name = "ЗакрытьAIMPToolStripMenuItem"
+        Me.ЗакрытьAIMPToolStripMenuItem.Size = New System.Drawing.Size(97, 20)
+        Me.ЗакрытьAIMPToolStripMenuItem.Text = "Закрыть AIMP"
+        '
+        'ВыходToolStripMenuItem
+        '
+        Me.ВыходToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.ВыходToolStripMenuItem.Name = "ВыходToolStripMenuItem"
+        Me.ВыходToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.ВыходToolStripMenuItem.Text = "Выход"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ЗакрытьAIMPToolStripMenuItem, Me.ВыходToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 67)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.MenuStrip1.Size = New System.Drawing.Size(164, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'btnPlay
+        '
+        Me.btnPlay.BackgroundImage = Global.AimpControls.My.Resources.Resources.btn_play
+        Me.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnPlay.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnPlay.FlatAppearance.BorderSize = 0
+        Me.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPlay.Location = New System.Drawing.Point(42, 12)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(24, 24)
+        Me.btnPlay.TabIndex = 5
+        Me.btnPlay.TabStop = False
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.BackgroundImage = Global.AimpControls.My.Resources.Resources.btn_stop
+        Me.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnStop.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnStop.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnStop.FlatAppearance.BorderSize = 0
+        Me.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStop.Location = New System.Drawing.Point(12, 12)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(24, 24)
+        Me.btnStop.TabIndex = 4
+        Me.btnStop.TabStop = False
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnPause
+        '
+        Me.btnPause.BackgroundImage = Global.AimpControls.My.Resources.Resources.btn_pause
+        Me.btnPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnPause.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnPause.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnPause.FlatAppearance.BorderSize = 0
+        Me.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPause.Location = New System.Drawing.Point(72, 12)
+        Me.btnPause.Name = "btnPause"
+        Me.btnPause.Size = New System.Drawing.Size(24, 24)
+        Me.btnPause.TabIndex = 3
+        Me.btnPause.TabStop = False
+        Me.btnPause.UseVisualStyleBackColor = True
+        '
+        'btnPrev
+        '
+        Me.btnPrev.BackgroundImage = Global.AimpControls.My.Resources.Resources.btn_prev
+        Me.btnPrev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnPrev.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnPrev.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnPrev.FlatAppearance.BorderSize = 0
+        Me.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPrev.Location = New System.Drawing.Point(102, 12)
+        Me.btnPrev.Name = "btnPrev"
+        Me.btnPrev.Size = New System.Drawing.Size(24, 24)
+        Me.btnPrev.TabIndex = 2
+        Me.btnPrev.TabStop = False
+        Me.btnPrev.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.BackgroundImage = Global.AimpControls.My.Resources.Resources.btn_next
+        Me.btnNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnNext.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnNext.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnNext.FlatAppearance.BorderSize = 0
+        Me.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnNext.Location = New System.Drawing.Point(132, 12)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(24, 24)
+        Me.btnNext.TabIndex = 0
+        Me.btnNext.TabStop = False
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(164, 91)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnPause)
+        Me.Controls.Add(Me.btnPrev)
+        Me.Controls.Add(Me.btnNext)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "MainForm"
+        Me.Opacity = 0.85R
+        Me.Text = "AimpControls"
+        Me.TopMost = True
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents ЗакрытьAIMPToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ВыходToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnPrev As Button
+    Friend WithEvents btnPause As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnPlay As Button
+End Class
---
+++ Forms/main_form.vb
@@ -0,0 +1,63 @@
+﻿Public Class MainForm
+    Public appPath As String = "AIMP.exe"
+
+    'ПРОЗРАЧНОСТЬ
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Me.Opacity = TrackBar1.Value / 100
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'ПРОГА ДОЛЖНА БЫТЬ В ПАПКЕ С ПЛЕЕРОМ
+        If My.Computer.FileSystem.FileExists(appPath) = False Then
+            MsgBox("Программа должна находиться в одно папке с AIMP.exe", vbCritical)
+            Application.Exit()
+        End If
+        'ГРУЗИМ НАСТРОЙКИ
+        Me.Opacity = My.Settings.Opacity
+        TrackBar1.Value = My.Settings.Trackbar
+        Me.Left = My.Settings.PositionX
+        Me.Top = My.Settings.PositionY
+    End Sub
+
+    'СОХРАНЯЕМ СОСТОЯНИЕ
+    Private Sub MainForm_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        My.Settings.Opacity = Me.Opacity
+        My.Settings.Trackbar = TrackBar1.Value
+        My.Settings.PositionX = Me.Left
+        My.Settings.PositionY = Me.Top
+    End Sub
+
+    'КНОПКИ
+    Function playerControls(appArg As String) As String
+        Shell(appPath + " " + appArg)
+    End Function
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        Call playerControls("/stop")
+    End Sub
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        Call playerControls("/play")
+    End Sub
+
+    Private Sub btnPause_Click(sender As Object, e As EventArgs) Handles btnPause.Click
+        Call playerControls("/pause")
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        Call playerControls("/next")
+    End Sub
+
+    Private Sub btnPrev_Click(sender As Object, e As EventArgs) Handles btnPrev.Click
+        Call playerControls("/prev")
+    End Sub
+
+    'МЕНЮ
+    Private Sub ЗакрытьAIMPToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ЗакрытьAIMPToolStripMenuItem.Click
+        Call playerControls("/exit")
+    End Sub
+
+    Private Sub ВыходToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ВыходToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+End Class
